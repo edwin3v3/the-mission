fc6c1fd4fe5e03cd48b6510ee30fe29139e24179
sharon <njerisharon84@gmail.com>
2025-07-24 12:00:01 +0300

    Added form components

--- src/components/AddMission.jsx
+++ src/components/AddMission.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import { useState } from "react";
 
 function AddMission() {
-  const [mission, setMission] = React.useState("");
-  const [missions, setMissions] = React.useState([]);
-
+    const [missions, setMissions] = React.useState([]);
+    const [formData, setFormData] = useState({
+        codeName: "",
+        objective: "",
+        priority: "",
+        misson_status: ""
+    });
   const handleAddMission = () => {
     if (mission.trim()) {
       setMissions([...missions, mission]);
@@ -15,13 +19,41 @@ function AddMission() {
   return (
     <div>
       <h2>Add a New Mission</h2>
-      <input
-        type="text"
-        value={mission}
-        onChange={(e) => setMission(e.target.value)}
-        placeholder="Enter mission"
-      />
-      <button onClick={handleAddMission}>Add Mission</button>
+      <form onSubmit={sendData}>
+        <input
+          type="text"
+          value={codeName}
+          onChange={(e) => {setFormData({...formData, codeName: e.target.value})}}
+          placeholder="Enter mission codename"
+          required
+        />
+        <input
+          type="text"
+          value={objective}
+          onChange={(e) => {setFormData({...formData, objective: e.target.value})}} 
+            placeholder="Enter mission objective"
+          required 
+        />
+        <select       value={priority}
+          onChange={(e) => setPriority(e.target.value)}
+          required
+        >
+          <option value="">Select priority</option>
+          <option value="High">High</option>    
+            <option value="Medium">Medium</option>      
+            <option value="Low">Low</option>
+                    </select>
+        <input
+          type="text"
+          value={mission_status}
+          onChange={(e) => {setFormData({...formData, mission_status: e.target.value})}}
+          placeholder="Enter mission status" 
+            required        
+        />  
+                    
+        <button type="submit">Add Mission</button>
+      </form>
+      
       <ul>
         {missions.map((m, index) => (
           <li key={index}>{m}</li>
